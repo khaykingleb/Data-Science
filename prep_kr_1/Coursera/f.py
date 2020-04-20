4c01a1a8eac6c7531816a10242b6b4ae9feb1d65
khaykingleb <khaykingleb@users.noreply.github.com>
2020-04-20 14:34:29 +0300

    added easy algorithms

--- prep_kr_1/Coursera/f.py
+++ prep_kr_1/Coursera/f.py
@@ -1,3 +1,16 @@
-n = int(input())
-lst = [i for i in range(int('1' + '0' * (n - 1)), int('1' + '0' * n)) if i % 2 != 0]
-print(*list(reversed(lst)))
+lst = list(map(int, input().split()))
+
+
+def max_product(A):
+    max_prod = A[0] * A[1]
+    k = A[0]
+    m = A[1]
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            if A[i] * A[j] > max_prod:
+                k = A[i]
+                m = A[j]
+    return k, m
+
+
+print(*sorted(max_product(lst)))
